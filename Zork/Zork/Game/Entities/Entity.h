82d4f4984d848c96296e6349cef125ee4a4fb503
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-16 19:30:34 +0200

    Entity base structure

---
+++ Zork/Zork/Game/Entities/Entity.h
@@ -0,0 +1,23 @@
+#ifndef _ENTITY_INCLUDE
+#define _ENTITY_INCLUDE
+
+#include <string>
+#include <list>
+using namespace std;
+
+class Entity
+{
+public:
+	enum EntityType {
+		NUM_TYPES
+	};
+
+	virtual void Update();
+
+private:
+	string name;
+	string description;
+	list<Entity*> contains;
+};
+
+#endif // _ENTITY_INCLUDE
