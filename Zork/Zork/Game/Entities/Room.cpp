f4bb764abbe75f366a77f33acca2ff4713ce9890
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-17 00:16:38 +0200

    added room name to output of Room::Look

--- Zork/Zork/Game/Entities/Room.cpp
+++ Zork/Zork/Game/Entities/Room.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Room.h"
 
 Room::Room(const string& name, const string& description) : Entity(name, description)
@@ -11,6 +12,7 @@ Room::~Room()
 
 void Room::Look() const
 {
+	std::cout << name << endl;
 	Entity::Look();
 	for (Entity* entity : contains) {
 		entity->Look();
