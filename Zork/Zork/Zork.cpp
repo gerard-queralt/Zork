9a40618846ea7a878166ce45bc88cc5aba6bb014
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-16 21:49:53 +0200

    main reads input

--- Zork/Zork/Zork.cpp
+++ Zork/Zork/Zork.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <string>
 #include "Game/World.h"
+
+using namespace std;
 
 int main()
 {
     World world;
-    world.Init();
+    string args;
     
     while (true) {
+        cout << "> ";
+        getline(cin, args);
         world.Update();
     }
 
