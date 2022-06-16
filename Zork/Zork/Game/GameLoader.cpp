7b83890298e6cb2776c364c8e1c3007bba6c5674
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-16 23:56:33 +0200

    GameLoader creates one room

--- Zork/Zork/Game/GameLoader.cpp
+++ Zork/Zork/Game/GameLoader.cpp
@@ -1,10 +1,14 @@
 #include "GameLoader.h"
+#include "Entities/Room.h"
+#include "Entities/Player.h"
 
 std::vector<Entity*> GameLoader::LoadEntities()
 {
     std::vector<Entity*> entities;
 
+    Room* start = new Room("West of House", "This is an open field west of a white house, with a boarded front door.");
 
+    entities.push_back(start);
 
     return entities;
 }
