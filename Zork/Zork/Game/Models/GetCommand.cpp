14b2003ab5bd4a567cec79001c1b787acb424ea0
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-17 23:42:20 +0200

    check if item can be picked in GetCommand

--- Zork/Zork/Game/Models/GetCommand.cpp
+++ Zork/Zork/Game/Models/GetCommand.cpp
@@ -13,8 +13,13 @@ GetCommand::~GetCommand()
 void GetCommand::Do(Player* player)
 {
 	if (player->getLocation()->EntityInRoom(target->getName())) {
-		player->AddEntity(target);
-		cout << "Taken." << endl;
+		if (target->CanBePicked()) {
+			player->AddEntity(target);
+			cout << "Taken." << endl;
+		}
+		else {
+			cout << "That can't be picked up." << endl;
+		}
 	}
 	else {
 		cout << "That's not an item in this room." << endl;
