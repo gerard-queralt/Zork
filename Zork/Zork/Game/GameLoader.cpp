23f58cc53c52d1baa6f9560fdf275d398c6b2473
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-18 14:49:41 +0200

    added forest room with key on the ground

--- Zork/Zork/Game/GameLoader.cpp
+++ Zork/Zork/Game/GameLoader.cpp
@@ -30,9 +30,9 @@ vector<Item*> GameLoader::LoadItems()
 {
     vector<Item*> items;
 
-    Item* mailbox = new Item("Mailbox", "a small mailbox", false);
+    Item* houseKey = new Item("Key", "a small key.", true);
 
-    items.push_back(mailbox);
+    items.push_back(houseKey);
 
     return items;
 }
@@ -42,13 +42,16 @@ vector<Room*> GameLoader::LoadRooms(const vector<Entity*>& existingEntities)
     vector<Room*> rooms;
 
     Room* westHouse = new Room("West of House", "This is an open field west of a white house, with a boarded front door.");
-    Entity* mailbox = EntityFinder::FindEntityByName("Mailbox", existingEntities);
-    westHouse->AddEntity(mailbox);
 
     Room* livingRoom = new Room("Living Room", "You are in the living room.");
 
+    Room* forest = new Room("Forest", "This is a forest, with trees in all directions around you.\nYou feel like you shouldn't go further.");
+    Entity* houseKey = EntityFinder::FindEntityByName("Key", existingEntities);
+    forest->AddEntity(houseKey);
+
     rooms.push_back(westHouse);
     rooms.push_back(livingRoom);
+    rooms.push_back(forest);
 
     return rooms;
 }
@@ -59,11 +62,18 @@ vector<Exit*> GameLoader::LoadExits(const vector<Entity*>& existingEntities)
 
     Entity* westHouse = EntityFinder::FindEntityByName("West of House", existingEntities);
     Entity* livingRoom = EntityFinder::FindEntityByName("Living Room", existingEntities);
+    Entity* forest = EntityFinder::FindEntityByName("Forest", existingEntities);
     if (westHouse->getType() == ROOM && livingRoom->getType() == ROOM) {
-        Exit* westToLiving = new Exit("House door", "The door to the house seems open", EAST, (Room*)westHouse, (Room*)livingRoom);
+        Exit* westToLiving = new Exit("House door", "The door to the house seems open.", EAST, (Room*)westHouse, (Room*)livingRoom);
         westHouse->AddEntity(westToLiving);
         livingRoom->AddEntity(westToLiving->Reverse());
         exits.push_back(westToLiving);
+    }
+    if (westHouse->getType() == ROOM && forest->getType() == ROOM) {
+        Exit* westToForest = new Exit("Entrance Forest", "There is a faded path between two trees.", WEST, (Room*)westHouse, (Room*)forest);
+        westHouse->AddEntity(westToForest);
+        forest->AddEntity(westToForest->Reverse());
+        exits.push_back(westToForest);
     }
 
     return exits;
