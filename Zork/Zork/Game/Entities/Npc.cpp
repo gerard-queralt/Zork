af141e150165197e0cf422be5cb4dc4f3a2f4498
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-19 22:12:43 +0200

    npc adds itself to its location

--- Zork/Zork/Game/Entities/Npc.cpp
+++ Zork/Zork/Game/Entities/Npc.cpp
@@ -4,6 +4,7 @@
 Npc::Npc(const string& i_name, const string& i_description, Room* i_location, int i_maxHP) : Creature(i_name, i_description, i_location, i_maxHP)
 {
 	m_type = NPC;
+	i_location->AddEntity(this);
 }
 
 Npc::~Npc()
