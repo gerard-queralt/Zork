59ebc5bed02bd9f028d72e3c14410d4d5f3e16c4
gerard-queralt <gerard.queralt@estudiantat.upc.edu>
2022-06-20 00:11:46 +0200

    added small intro at the beginning of the game

--- Zork/Zork/Zork.cpp
+++ Zork/Zork/Zork.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 int main()
 {
+    cout << "Welcome to my small tribute of the game Zork. I hope you'll enjoy it!" << endl;
+    cout << "Your objective is to slay a monster lurking beneath a house in the middle of the woods." << endl;
+    cout << "But be careful! Even if it's slow, it's very tough. You will most likely need a special weapon to defeat it." << endl;
+    cout << "For more information about the game or the commands you can input, please visit https://github.com/gerard-queralt/Zork." << endl;
+    cout << endl << endl;
+
     World world;
     string args;
     
